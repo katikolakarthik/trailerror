8f0b31e5f7766e6fb6e3fd07b9ac734e72f98694
katikolakarthik <154609029+katikolakarthik@users.noreply.github.com>
2024-11-26 16:44:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ app.secret_key = "your_secret_key"
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = '22K91A6777@tkrcet.com'  # Replace with your email
-app.config['MAIL_PASSWORD'] = 'tkrcet@2022'  # Replace with your email password
+app.config['MAIL_USERNAME'] = 'katikolakarthik@gmail.com'  # Replace with your email
+app.config['MAIL_PASSWORD'] = 'Karthik@4477'  # Replace with your email password
 
 mail = Mail(app)
 
